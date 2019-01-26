186e0776115502a581fa168d949468cf2448d6a2
nmtjhngrv <30773461+nmtjhngrv@users.noreply.github.com>
2019-01-26 06:33:46 +0300

    Bank ATM
    Little project

---
+++ Bank ATM Operations.c
@@ -0,0 +1,74 @@
+#include<stdio.h>
+
+int main() {
+	int operation;
+	int balance=1000;
+	int amount;
+	
+	printf("Operations\n 1:Cash Money\n 2:Deposit Money\n 3:EFT\n 4:Balance\n 5:Card Refund\n\n\n");
+	
+	printf("Choose Operations:");
+	scanf("%d", &operation);
+		
+	switch(operation){
+		
+		    case 1:
+		    	printf("Your Balance:%d\n",balance);
+		    	printf("Cash Amount :");
+		    	scanf("%d",&amount);
+		if(amount>balance){
+			   printf("You balance is not enough\n");
+		   }
+		    balance-=amount;
+		    printf("Balance: %d",balance);
+		        
+		break;
+			
+			case 2:
+			    printf("Your Balance:%d\n",balance);
+		    	printf("Deposit Money:");
+		    	scanf("%d",&amount);
+			
+			 balance+=amount;
+		    printf("Balance: %d",balance);
+	    break;
+			
+			
+			case 3:
+			printf("Your Balance:%d\n",balance);
+		    	printf("EFT:");
+		    	scanf("%d",&amount);
+		if(amount>balance){
+			   printf("You balance is not enough\n");
+		   }
+		    balance-=amount;
+		    printf("Balance: %d",balance);
+		  
+		break;
+		
+			case 4:
+			
+		printf("Balance %d",balance);
+		break;
+			
+			case 5:
+				
+		printf("Card Refunded:");		
+	    break;
+		
+		
+		
+			default:
+			printf("Unknown Operation:");
+			
+			
+		
+		
+	}
+	
+	
+	
+
+
+	return 0;
+}
